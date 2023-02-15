eab519b6ad73562658925135fe5da21240255e61
Oleksii Hayvoronskyi <oleksiihayvoronskyi@gmail.com>
2023-02-15 21:31:31 +0200

    done

--- task_1.py
+++ task_1.py
@@ -1,13 +1,12 @@
-# Заваання 1. Написати клас для роботи з матрицями.
+# Завдання 1. Написати клас для роботи з матрицями.
 # Клас повинен створювати об’єкт матриць з вкладених списків,
 # виводити матриці на друк, виконувати операції додавання, віднімання,
 # множення на число, множення на матрицю, транспонування.
 # Передбачити можливість приведення матриць для операцій додавання
-# і віднімання, а також обробку виключних ситуацій для операції множення
-# матриці на матрицю.
+# і віднімання.
 
 
-print ('--- Task 1 ---')
+print('--- Task 1 ---')
 
 
 # Створюю клас для роботи з матрицями.
@@ -45,45 +44,48 @@ class Matrix:
     def transpose(self, matrix):
         return [list(i) for i in zip(*matrix)]
 
-    def getTranspose(self):
+    def get_transpose(self):
         return self.get_readable_matrix_string(self.transpose(self.matrix))
 
     # Метод для множення матриці на матрицю.
     def multiply(self, matrix):
         mtrx_mult = [[0 for j in range(len(matrix[i]))] for i in
-                  range(len(self.matrix))]
+                     range(len(self.matrix))]
         for i in range(len(self.matrix)):
             for j in range(len(matrix[0])):
                 for k in range(len(matrix)):
                     mtrx_mult[i][j] += self.matrix[i][k] * matrix[k][j]
         return mtrx_mult
 
-    def getMultiply(self, matrix):
+    def get_multiply(self, matrix):
         return self.get_readable_matrix_string(self.multiply(matrix))
 
     # Метод для ділення матриці на матрицю.
     def divide(self, matrix):
-        mtrx_div = [[0 for j in range(len(matrix[i]))] for i in
-                  range(len(self.matrix))]
-        for i in range(len(self.matrix)):
-            for j in range(len(matrix[0])):
-                for k in range(len(matrix)):
-                    mtrx_div[i][j] += self.matrix[i][k] / matrix[k][j]
-        return mtrx_div
+            mtrx_div = [[0 for j in range(len(matrix[i]))] for i in
+                        range(len(self.matrix))]
+            for i in range(len(self.matrix)):
+                for j in range(len(matrix[0])):
+                    for k in range(len(matrix)):
+                        mtrx_div[i][j] += self.matrix[i][k] / matrix[k][j]
+            return mtrx_div
 
-    def getDivide(self, matrix):
+    def get_divide(self, matrix):
         return self.get_readable_matrix_string(self.divide(matrix))
 
     # Метод для додавання матриць.
     def addition(self, matrix):
-        mtrx_add = [[0 for j in range(len(matrix[i]))] for i in
-                    range(len(self.matrix))]
-        for i in range(len(self.matrix)):
-            for j in range(len(matrix[0])):
+        try:
+            mtrx_add = [[0 for j in range(len(matrix[i]))] for i in
+                        range(len(self.matrix))]
+            for i in range(len(self.matrix)):
+                for j in range(len(matrix[0])):
                     mtrx_add[i][j] += self.matrix[i][j] + matrix[i][j]
-        return mtrx_add
+            return mtrx_add
+        except Exception as err:
+            print('Unexpected value', err)
 
-    def getAddition(self, matrix):
+    def get_addition(self, matrix):
         return self.get_readable_matrix_string(self.addition(matrix))
 
     # Метод для віднімання матриць.
@@ -95,7 +97,7 @@ class Matrix:
                 mtrx_sub[i][j] += self.matrix[i][j] - matrix[i][j]
         return mtrx_sub
 
-    def getSubtracting(self, matrix):
+    def get_subtracting(self, matrix):
         return self.get_readable_matrix_string(self.subtracting(matrix))
 
     # Метод для множення матриці на число.
@@ -106,37 +108,37 @@ class Matrix:
             [[num * other for num in row] for row in self.matrix])
 
 
-# Створюю матрицю 1.
-mtrx_1 = Matrix(2, 3)
-print('Matrix 1:')
-print(mtrx_1)
+if __name__ == '__main__':
+    # Створюю матрицю 1.
+    mtrx_1 = Matrix(5, 6)
+    print('Matrix 1:')
+    print(mtrx_1)
 
-# Транспонування матриці.
-print('\nTranspose matrix 1:')
-print(mtrx_1.getTranspose())
+    # Транспонування матриці.
+    print('\nTranspose matrix 1:')
+    print(mtrx_1.get_transpose())
 
-# Створюю матрицю 2.
-mtrx_2 = Matrix(2, 3)
-print('\nMatrix 2:')
-print(mtrx_2)
+    # Створюю матрицю 2.
+    mtrx_2 = Matrix(5, 6)
+    print('\nMatrix 2:')
+    print(mtrx_2)
 
-# Множення матриці на матрицю.
-print('\nMultiplication (mtrx_2 * mtrx_1):')
-print(mtrx_2.getMultiply(mtrx_1))
+    # Множення матриці на матрицю.
+    print('\nMultiplication (mtrx_2 * mtrx_1):')
+    print(mtrx_2.get_multiply(mtrx_1))
 
-# Ділення матриці на матрицю.
-print('\nDivide (mtrx_2 / mtrx_1):')
-print(mtrx_2.getDivide(mtrx_1))
+    # Ділення матриці на матрицю.
+    print('\nDivide (mtrx_2 / mtrx_1):')
+    print(mtrx_2.get_divide(mtrx_1))
 
-# Додаваня матриці 2 до матриц 1.
-print('\nAddition (mtrx_2 + mtrx_1):')
-print(mtrx_2.getAddition(mtrx_1))
+    # Додаваня матриці 2 до матриц 1.
+    print('\nAddition (mtrx_2 + mtrx_1):')
+    print(mtrx_2.get_addition(mtrx_1))
 
-# Віднімання матриці 2 від матриці 1.
-print('\nSubtracting (mtrx_2 - mtrx_1):')
-print(mtrx_2.getSubtracting(mtrx_1))
+    # Віднімання матриці 2 від матриці 1.
+    print('\nSubtracting (mtrx_2 - mtrx_1):')
+    print(mtrx_2.get_subtracting(mtrx_1))
 
-# Множення матриці на число.
-print('\nMatrix 1 multiplication by 3:')
-print(mtrx_1 * 3)
-
+    # Множення матриці на число.
+    print('\nMatrix 1 multiplication by 3:')
+    print(mtrx_1 * 3)
